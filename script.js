8480f2709f4722201347382713f1435a1aa49f92
AnkitGowda <ankithgowdaankithgowda129@gmail.com>
2024-01-04 21:19:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// let test2="i am student";
+
+//  console.log(test2);
+
+// const num="learner";
+// console.log(num);
+
+// {
+//     let r="roshan"
+//     console.log(r)
+// }
+// {
+//     var p="pranava";
+// }
+// console.log(p);
+
+// const arr=[0,1,2,3,4,5];
+
+// arr.push(200)
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.pop();
+
+// console.log(arr)
+// 
+
+// const arr=[1,2,3,4,5,6];
+
+// console.log(arr)
+
+// const data=["hii","hello","my world", "js","python","full stack"]
+// const a=data[0];
+
+// console.log(data[0]);
+
+// console.log(data[1]);
+
+// console.log(data[2]);
+
+// console.log(data[3]);
+
+// console.log(data[4]);
+
+// console.log(data[5]);
+
+// 
+
+
+
+// let person = {
+//     name: 'John',
+//     age: 30,
+//     job: 'developer'
+//   };
+  
+//   for (let key in person) {
+//     console.log(key + ': ' + person[key]);
+//   }
+  
+// const [a,b,c]=["windows","linux","ios","chrome","fire fox"];
+
+// const arr=[...a];
+
+// console.log(arr);
+
+// 
+const obj={
+    name:"shubhang",
+    usn:"4al21cs080",
+    hobies:"all in one"
+}
+
+console.log(obj.name.usn.hobies)
