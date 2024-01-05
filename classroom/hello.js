639ac1664d7b9615b6848488259438b8161c1488
AnkitGowda <137199666+AnkitGowda@users.noreply.github.com>
2024-01-05 22:30:49 +0530

    Add files via upload

---
+++ classroom/hello.js
@@ -0,0 +1,52 @@
+//Assignment operator
+// let a1 = 3;
+//     a2 = 4;
+//     a3 = 5;
+//     a4 = 7;
+//     a5 = 6;
+//     a6 = 8;
+// let b = 4;
+//  a1 += 4;
+//  a2 -= 4;
+//  a3 *= 4;
+//  a4 /= 4;
+//  a5 %= 4;
+//  a6 **=4;
+
+//  console.log("a1+=",a1, "a2-=",a2,"a3*=",a3,"a4/=",a4,"a5%=",a5,"a6**=",a6 );
+ 
+// let a = 5;
+// let b = 5;
+//  let cond1 = a==b;
+//  let cond2 = a>=b;
+
+// console.log("cond1&&cond2",cond1&&cond2);
+
+// let age = 16;
+// if (age>18);{
+//     console.log("you can vote");
+// }
+// if (ade<18);{
+//     console.log("you cannot vote");
+// }
+// let mode = "dark";
+// let color;
+// if (mode==="dark"){
+//     color="black";
+// }
+// if (mode==="light"){
+//     color="light";
+// }
+
+// console.log(color);
+// let age = 30;
+// if (age>=18);{
+//     console.log("vote");
+// }else {
+//     console.log("can vote");
+// }
+let name = prompt("hello!");
+console.log(name);
+
+
+
